554a5dbf0ff693a1cc18b6a58feca641c1493875
ridhoadtyaa <r2603an@gmail.com>
2022-07-22 21:11:12 +0700

    feat: add login pages

---
+++ src/pages/login.tsx
@@ -0,0 +1,115 @@
+import Button from '@/components/UI/buttons/Button'
+import NextImage from '@/components/UI/image/NextImage'
+import UnstyledLink from '@/components/UI/links/UnstyedLink'
+import ErrorMessage from '@/components/forms/ErrorMessage'
+import Input from '@/components/forms/Input'
+
+import { clsxm } from '@/lib'
+import { loginSchema } from '@/utils'
+
+import { yupResolver } from '@hookform/resolvers/yup'
+import { NextPage } from 'next'
+import { SubmitHandler, useForm } from 'react-hook-form'
+import { MdEmail, MdLock } from 'react-icons/md'
+
+type LoginForm = {
+  email: string
+  password: string
+}
+
+const Login: NextPage = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm<LoginForm>({
+    resolver: yupResolver(loginSchema),
+    shouldFocusError: true
+  })
+
+  const onSubmit: SubmitHandler<LoginForm> = (data) => console.log(data)
+
+  return (
+    <div className={clsxm('bg-blue-100/50 min-h-screen', 'flex items-center')}>
+      <div className={clsxm('max-w-lg mx-auto w-11/12 sm:w-full', 'py-8')}>
+        <UnstyledLink href='/'>
+          <NextImage
+            src='/img/nuwszy-logo.png'
+            width={50}
+            height={50}
+            alt='Nuwszy Logo'
+            className={clsxm('mx-auto', 'mb-12')}
+          />
+        </UnstyledLink>
+        <div className={clsxm('bg-white rounded-md', 'py-12 px-10')}>
+          <h3 className={clsxm('text-primary font-medium text-center', 'mb-3')}>Welcome Back</h3>
+          <p className={clsxm('text-center text-slate-400/80', 'mb-12')}>
+            Enter your crendetials to access your account
+          </p>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div className={clsxm('relative', 'mb-8')}>
+              <Input
+                placeholder='Enter your email'
+                type='email'
+                className={clsxm(
+                  'focus:ring focus:ring-blue-300 border border-blue-100',
+                  errors.email?.message && 'border-red-400 focus:border-red-400 focus:ring-red-400',
+                  'placeholder:text-slate-400 md:placeholder:text-sm',
+                  'rounded-sm w-full',
+                  'py-2 pl-12',
+                  'mb-2'
+                )}
+                {...register('email')}
+              />
+              <MdEmail
+                className={clsxm('text-blue-400', 'absolute top-[9.5px] left-2')}
+                size={23}
+              />
+              <ErrorMessage message={errors.email?.message} />
+            </div>
+            <div className={clsxm('relative', 'mb-10')}>
+              <Input
+                placeholder='Enter your password'
+                type='password'
+                className={clsxm(
+                  'focus:ring focus:ring-blue-300 border border-blue-100',
+                  errors.password?.message &&
+                    'border-red-400 focus:border-red-400 focus:ring-red-400',
+                  'placeholder:text-slate-400 md:placeholder:text-sm',
+                  'rounded-sm w-full',
+                  'py-2 pl-12',
+                  'mb-2'
+                )}
+                {...register('password')}
+              />
+              <MdLock className={clsxm('text-blue-400', 'absolute top-[9.5px] left-2')} size={23} />
+              <ErrorMessage message={errors.password?.message} />
+            </div>
+            <Button
+              type='submit'
+              className={clsxm(
+                'focus:ring focus:ring-blue-400 focus:outline-none',
+                'bg-blue-500 rounded-lg hover:bg-blue-600',
+                'text-white font-medium',
+                'transition duration-300',
+                'w-full block',
+                'py-3'
+              )}
+            >
+              Login
+            </Button>
+          </form>
+        </div>
+        <UnstyledLink
+          href='/register'
+          className={clsxm('text-slate-400  text-center', 'mt-8', 'block')}
+        >
+          Don&apos;t have an account ?{' '}
+          <span className={clsxm('text-blue-500 font-medium')}>Register</span>
+        </UnstyledLink>
+      </div>
+    </div>
+  )
+}
+
+export default Login
