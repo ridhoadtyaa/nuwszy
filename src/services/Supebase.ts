a84f90de243aed4a269a392d5356c63e32030e77
ridhoadtyaa <r2603an@gmail.com>
2022-07-28 16:48:48 +0700

    feat: add sign up func

--- src/services/Supebase.ts
+++ src/services/Supebase.ts
@@ -1,3 +1,5 @@
+import { CustomError } from '@/utils'
+
 import { SUPABASE_ANON_KEY, SUPABASE_URL } from './env'
 
 import { createClient } from '@supabase/supabase-js'
@@ -7,33 +9,57 @@ export const supabase = createClient(SUPABASE_URL as string, SUPABASE_ANON_KEY a
 
 export const signIn = async (payload: SigninUserPayload) => {
   try {
-    const { user, error } = await supabase.auth.signIn({
+    const { error } = await supabase.auth.signIn({
       email: payload.email,
       password: payload.password
     })
 
-    return user
+    if (error) throw new CustomError(error)
+
+    return true
   } catch (error) {
     console.log((error as Error).message)
+    return false
   }
 }
 
 export const signUp = async (payload: SignupUserPayload) => {
   try {
-    const { user, error } = await supabase.auth.signUp({
-      email: payload.email,
-      password: payload.password
-    })
+    const { error } = await supabase.auth.signUp(
+      {
+        email: payload.email,
+        password: payload.password
+      },
+      { data: { name: payload.name } }
+    )
 
-    return user
+    if (error) throw new CustomError(error)
+
+    return true
   } catch (error) {
     console.log((error as Error).message)
+    return false
+  }
+}
+
+export const signOut = async () => {
+  try {
+    const { error } = await supabase.auth.signOut()
+
+    if (error) throw new CustomError(error)
+
+    return true
+  } catch (error) {
+    console.log((error as Error).message)
+    return false
   }
 }
 
 export const getAllPosts = async () => {
   try {
     const { data, error } = await supabase.from<Post[]>('posts').select()
+
+    if (error) throw new CustomError(error)
 
     return data
   } catch (error) {
@@ -44,6 +70,8 @@ export const getAllPosts = async () => {
 export const getDetailPost = async (slug: string) => {
   try {
     const { data, error } = await supabase.from<Post>('posts').select().eq('slug', slug).single()
+
+    if (error) throw new CustomError(error)
 
     return data
   } catch (error) {
