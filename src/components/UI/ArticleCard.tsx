1ea9248a42e6dff7f0ff9f0a455b4f149c7add82
ridhoadtyaa <r2603an@gmail.com>
2022-07-28 16:43:51 +0700

    feat: remove div

--- src/components/UI/ArticleCard.tsx
+++ src/components/UI/ArticleCard.tsx
@@ -26,42 +26,42 @@ const ArticleCard: React.FunctionComponent<ArticleCardProps> = ({
   } as Intl.DateTimeFormatOptions
 
   return (
-    <div className={clsxm('py-12', 'border-y border-sky-100')}>
-      <article className={clsxm('flex flex-col-reverse items-center sm:flex-row', 'sm:space-x-16')}>
-        <div className={clsxm('text-primary', 'w-full sm:w-8/12')}>
-          <div className={clsxm('inline-flex items-center', 'space-x-5', 'mt-7 mb-7 sm:mt-0')}>
-            <div
-              className={clsxm(
-                'bg-sky-100/70',
-                'rounded-l-md',
-                'py-2 px-4',
-                'font-semibold text-sm'
-              )}
-            >
-              {category}
-            </div>
-            <div className={clsxm('text-sm font-medium')}>{dateFormat(date, 'en-US', config)}</div>
+    <article
+      className={clsxm(
+        'flex flex-col-reverse items-center sm:flex-row',
+        'sm:space-x-16',
+        'py-12',
+        'border-y border-sky-100'
+      )}
+    >
+      <div className={clsxm('text-primary', 'w-full sm:w-8/12')}>
+        <div className={clsxm('inline-flex items-center', 'space-x-5', 'mt-7 mb-7 sm:mt-0')}>
+          <div
+            className={clsxm('bg-sky-100/70', 'rounded-l-md', 'py-2 px-4', 'font-semibold text-sm')}
+          >
+            {category}
           </div>
-          <div>
-            <UnstyledLink href={slug}>
-              <h2>{title}</h2>
-            </UnstyledLink>
-            <p className={clsxm('mt-5')}>{desc}</p>
-          </div>
+          <div className={clsxm('text-sm font-medium')}>{dateFormat(date, 'en-US', config)}</div>
         </div>
-        <NextImage
-          src={thumbnail}
-          width={100}
-          height={70}
-          objectFit='cover'
-          alt='test'
-          useSkeleton
-          blurClassName='bg-slate-100'
-          className={clsxm('w-full sm:w-4/12')}
-          imgClassName={clsxm('rounded-lg')}
-        />
-      </article>
-    </div>
+        <div>
+          <UnstyledLink href={slug}>
+            <h2>{title}</h2>
+          </UnstyledLink>
+          <p className={clsxm('mt-5')}>{desc}</p>
+        </div>
+      </div>
+      <NextImage
+        src={thumbnail}
+        width={100}
+        height={70}
+        objectFit='cover'
+        alt='test'
+        useSkeleton
+        blurClassName='bg-slate-100'
+        className={clsxm('w-full sm:w-4/12')}
+        imgClassName={clsxm('rounded-lg')}
+      />
+    </article>
   )
 }
 
