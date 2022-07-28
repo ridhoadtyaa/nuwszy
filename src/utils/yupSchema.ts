f9bb0f12229c1e675724a1e8ebf6aa3498f6fb57
ridhoadtyaa <r2603an@gmail.com>
2022-07-28 16:47:52 +0700

    feat: add register schema

--- src/utils/yupSchema.ts
+++ src/utils/yupSchema.ts
@@ -4,3 +4,9 @@ export const loginSchema = yup.object().shape({
   email: yup.string().required().email('Invalid email address'),
   password: yup.string().required()
 })
+
+export const registerSchema = yup.object().shape({
+  name: yup.string().required().min(5),
+  email: yup.string().required().email('Invalid email address'),
+  password: yup.string().required().min(6)
+})
