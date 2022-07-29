e4ef881a0116ad67471d48bec68fa7036e5d67b9
ridhoadtyaa <r2603an@gmail.com>
2022-07-29 21:24:34 +0700

    feat: add profile dashboard page

---
+++ src/pages/dashboard/profile.tsx
@@ -0,0 +1,14 @@
+import AuthLayout from '@/components/Layouts/AuthLayout'
+
+import { NextPage } from 'next'
+
+const Profile: NextPage = () => {
+  return (
+    <AuthLayout>
+      <h2>My Profile</h2>
+      <div></div>
+    </AuthLayout>
+  )
+}
+
+export default Profile
