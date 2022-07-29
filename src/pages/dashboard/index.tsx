4b1ea4a65d41b6508f90c20e341c78dd56c1bda7
ridhoadtyaa <r2603an@gmail.com>
2022-07-29 21:24:11 +0700

    chore: remove comment

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -5,18 +5,10 @@ import { NextPage } from 'next'
 const DashboardPage: NextPage = () => {
   return (
     <AuthLayout>
-      <h3>My Post</h3>
+      <h2>My Post</h2>
       <div></div>
     </AuthLayout>
   )
 }
-
-// export const getServerSideProps = async ({ req, res }) => {
-//   const { user } = await getUser({ req, res })
-
-//   console.log('[getServerSideProps] User:', user)
-
-//   return { props: {} }
-// }
 
 export default DashboardPage
