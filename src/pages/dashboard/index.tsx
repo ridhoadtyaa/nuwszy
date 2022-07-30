67052dd743aa0f5188c03865be06b0fa9b65d6db
ridhoadtyaa <r2603an@gmail.com>
2022-07-30 23:03:07 +0700

    feat: add page add post

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,12 +1,20 @@
 import AuthLayout from '@/components/Layouts/AuthLayout'
+import PrimaryButton from '@/components/UI/buttons/PrimaryButton'
+
+import { clsxm } from '@/lib'
 
 import { NextPage } from 'next'
+import Link from 'next/link'
 
 const DashboardPage: NextPage = () => {
   return (
     <AuthLayout>
-      <h2>My Post</h2>
-      <div></div>
+      <div className={clsxm('flex justify-between items-center')}>
+        <h2>My Post</h2>
+        <Link href='/dashboard/post/add-post'>
+          <PrimaryButton>Add Post</PrimaryButton>
+        </Link>
+      </div>
     </AuthLayout>
   )
 }
