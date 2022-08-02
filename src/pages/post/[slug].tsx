38852948ac0cd83054812e73d36da8e6866669ac
ridhoadtyaa <r2603an@gmail.com>
2022-08-02 21:09:04 +0700

    chore: comment server side props

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -1,24 +1,23 @@
-import { getDetailPost } from '@/services/Supebase'
-
-import type { GetServerSideProps, NextPage } from 'next'
+// import { getDetailPost } from '@/services/Supebase'
+import type { NextPage } from 'next'
 import { Post } from 'nuwszy'
 
 type PostDetailPageProps = {
   post: Post
 }
 
-const PostDetailPage: NextPage<PostDetailPageProps> = ({ post }) => {
-  return <h1>{post.title}</h1>
+const PostDetailPage: NextPage<PostDetailPageProps> = () => {
+  return <h1>Title post</h1>
 }
 
-export const getServerSideProps: GetServerSideProps = async (ctx) => {
-  const { slug } = ctx.params
+// export const getServerSideProps: GetServerSideProps = async (ctx) => {
+//   const { slug } = ctx.params
 
-  const post = await getDetailPost(slug)
+//   const post = await getDetailPost(slug)
 
-  return {
-    props: { post }
-  }
-}
+//   return {
+//     props: { post }
+//   }
+// }
 
 export default PostDetailPage
