871e4fdeb3fdaf8e8797bad199d07299cc1ff209
ridhoadtyaa <r2603an@gmail.com>
2022-08-02 21:09:50 +0700

    feat: add register user, and apply react hot toast

--- src/services/Supebase.ts
+++ src/services/Supebase.ts
@@ -4,10 +4,14 @@ import { SUPABASE_ANON_KEY, SUPABASE_URL } from './env'
 
 import { createClient } from '@supabase/supabase-js'
 import { Post, SigninUserPayload, SignupUserPayload } from 'nuwszy'
+import toast from 'react-hot-toast'
+import slugify from 'slugify'
+import { v4 as uuidv4 } from 'uuid'
 
 export const supabase = createClient(SUPABASE_URL as string, SUPABASE_ANON_KEY as string)
 
 export const signIn = async (payload: SigninUserPayload) => {
+  const toastId = toast.loading('Waiting...')
   try {
     const { error } = await supabase.auth.signIn({
       email: payload.email,
@@ -16,14 +20,19 @@ export const signIn = async (payload: SigninUserPayload) => {
 
     if (error) throw new CustomError(error)
 
+    toast.success('Login successful')
     return true
   } catch (error) {
-    console.log((error as Error).message)
+    error instanceof Error && toast.error(error.message)
+
     return false
+  } finally {
+    toast.remove(toastId)
   }
 }
 
 export const signUp = async (payload: SignupUserPayload) => {
+  const toastId = toast.loading('Waiting...')
   try {
     const { error } = await supabase.auth.signUp(
       {
@@ -35,23 +44,82 @@ export const signUp = async (payload: SignupUserPayload) => {
 
     if (error) throw new CustomError(error)
 
+    toast.success('Register successfully! Check your email to verify your account.', {
+      duration: 2000
+    })
     return true
   } catch (error) {
-    console.log((error as Error).message)
+    error instanceof Error && toast.error(error.message)
+
     return false
+  } finally {
+    toast.remove(toastId)
   }
 }
 
 export const signOut = async () => {
+  const toastId = toast.loading('Waiting...')
   try {
     const { error } = await supabase.auth.signOut()
 
     if (error) throw new CustomError(error)
 
+    toast.success('Logout successful')
+
     return true
   } catch (error) {
-    console.log((error as Error).message)
+    error instanceof Error && toast.error(error.message)
+
     return false
+  } finally {
+    toast.remove(toastId)
+  }
+}
+
+export const createPost = async (payload: Post, content: string, userId: string) => {
+  const toastId = toast.loading('Make a post...')
+  try {
+    if (!content) {
+      toast.error('Field content is required')
+      return
+    }
+
+    if (content.length < 200) return
+
+    const file = payload.thumbnail[0]
+
+    const uploadThumbnail = await supabase.storage
+      .from('thumbnails')
+      .upload(`${userId}-${uuidv4()}`, file, {
+        cacheControl: '3600',
+        upsert: true
+      })
+
+    const randomNumber = Math.floor(Math.random() * (999 - 100 + 1) + 100)
+
+    const uploadPost = await supabase.from<Post>('posts').insert([
+      {
+        ...payload,
+        thumbnail: uploadThumbnail.data?.Key,
+        content,
+        user_id: userId,
+        slug:
+          slugify(payload.title, { lower: true, locale: 'en' }) +
+          '-' +
+          userId.split('-')[0] +
+          randomNumber
+      }
+    ])
+
+    if (uploadPost.error) throw new CustomError(uploadPost.error)
+
+    toast.success('Post successfully created')
+    return uploadPost.data
+  } catch (error) {
+    error instanceof Error && toast.error(error.message)
+    return null
+  } finally {
+    toast.remove(toastId)
   }
 }
 
