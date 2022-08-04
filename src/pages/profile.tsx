6346e87f313b3a259846beb5257d1072ba796151
ridhoadtyaa <r2603an@gmail.com>
2022-08-04 22:01:14 +0700

    feat: add identity user to profile page

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,12 +1,23 @@
 import AuthLayout from '@/components/Layouts/AuthLayout'
 
+import useUser from '@/hooks/useUser'
+import { clsxm } from '@/lib'
+
 import { NextPage } from 'next'
 
 const Profile: NextPage = () => {
+  const user = useUser()
   return (
     <AuthLayout>
       <h2>My Profile</h2>
-      <div></div>
+      <section className={clsxm('mt-6')}>
+        <h4>
+          Name : <span className={clsxm('font-normal')}>{user?.name}</span>
+        </h4>
+        <h4>
+          Email : <span className={clsxm('font-normal')}>{user?.email}</span>
+        </h4>
+      </section>
     </AuthLayout>
   )
 }
