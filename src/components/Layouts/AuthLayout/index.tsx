cb30c3f3285b83b71143e2f10103ec5e115ecd62
ridhoadtyaa <r2603an@gmail.com>
2022-08-06 16:41:55 +0700

    feat: add title to auth layout

--- src/components/Layouts/AuthLayout/index.tsx
+++ src/components/Layouts/AuthLayout/index.tsx
@@ -5,14 +5,16 @@ import { supabase } from '@/services/Supebase'
 
 import Header from './Header'
 
+import Head from 'next/head'
 import { useRouter } from 'next/router'
 import { useEffect } from 'react'
 
 type AuthLayoutProps = {
   children: React.ReactNode
+  title?: string
 }
 
-const AuthLayout: React.FunctionComponent<AuthLayoutProps> = ({ children }) => {
+const AuthLayout: React.FunctionComponent<AuthLayoutProps> = ({ children, title }) => {
   const isUser = supabase.auth.user()
 
   const router = useRouter()
@@ -29,6 +31,9 @@ const AuthLayout: React.FunctionComponent<AuthLayoutProps> = ({ children }) => {
 
   return (
     <>
+      <Head>
+        <title>{title ?? 'Dashboard user'} &mdash; Nuwszy</title>
+      </Head>
       <Header />
 
       <main className={clsxm('px-10', 'mt-8')}>{children}</main>
