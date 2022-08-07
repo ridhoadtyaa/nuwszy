4040e84b54664695c65e758e5f9ca97afcd5b61a
ridhoadtyaa <r2603an@gmail.com>
2022-08-07 20:57:40 +0700

    feat: show posts by category page

--- src/pages/category/[category].tsx
+++ src/pages/category/[category].tsx
@@ -3,9 +3,12 @@ import ArticleCard from '@/components/UI/ArticleCard'
 import CategoryHeader from '@/components/UI/CategoryHeader'
 
 import { clsxm } from '@/lib'
+import { getPostsByCategory, getThumbnailPost } from '@/services/Supebase'
 
 import { NextPage } from 'next'
 import { useRouter } from 'next/router'
+import { Post } from 'nuwszy'
+import { useEffect, useMemo, useState } from 'react'
 
 type ServerSideParams = {
   params: {
@@ -13,26 +16,46 @@ type ServerSideParams = {
   }
 }
 
+const capitalizeWord = (word: string) => {
+  return word
+    .replace('-', ' ')
+    .toLowerCase()
+    .replace(/(^\w{1})|(\s{1}\w{1})/g, (match) => match.toUpperCase())
+}
+
 const Category: NextPage = () => {
   const router = useRouter()
+  const [posts, setPosts] = useState<Post[] | undefined>()
 
-  const categoryTitle = router.query.category as string
+  const category = router.query.category as string
+
+  const categoryTitle = useMemo(() => capitalizeWord(category), [category])
+
+  useEffect(() => {
+    ;(async () => {
+      const res = await getPostsByCategory(categoryTitle)
+
+      setPosts(res)
+    })()
+  }, [categoryTitle])
 
   return (
-    <MainLayout customHeader={<CategoryHeader title={categoryTitle} />}>
+    <MainLayout customHeader={<CategoryHeader title={category} />}>
       <section>
-        <h1 className={clsxm('capitalize text-primary', 'mb-6')}>
-          {categoryTitle?.replace('-', ' ')}
-        </h1>
-        <ArticleCard
-          category='DESIGN TOOLS'
-          date='2022-06-20 14:49:15+00'
-          title='10 Hilarious Cartoons That Depict Real-Life Problems of Programmers'
-          desc=' Redefined the user acquisition and redesigned the onboarding experience, all within 3
-              working weeks.'
-          thumbnail='https://assets.architecturaldigest.in/photos/60084dd6cce5700439e12bf7/16:9/w_2560%2Cc_limit/modern-living-room-decor-1366x768.jpg'
-          slug='test-test'
-        />
+        <h1 className={clsxm('capitalize text-primary', 'mb-6')}>{category?.replace('-', ' ')}</h1>
+        <div>
+          {posts?.map((post) => (
+            <ArticleCard
+              key={post.id}
+              category={post.category}
+              date={post.created_at}
+              title={post.title}
+              desc={post.description}
+              thumbnail={getThumbnailPost(post.thumbnail) as string}
+              slug={post.slug}
+            />
+          ))}
+        </div>
       </section>
     </MainLayout>
   )
