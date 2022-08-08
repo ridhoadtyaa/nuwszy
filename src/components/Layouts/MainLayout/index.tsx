6229491981c3e8605a53046fccdded8f54e828bd
ridhoadtyaa <r2603an@gmail.com>
2022-08-08 21:03:35 +0700

    feat: add seo setting to main layout

--- src/components/Layouts/MainLayout/index.tsx
+++ src/components/Layouts/MainLayout/index.tsx
@@ -1,17 +1,20 @@
+import Seo, { CustomSeoProps } from '@/components/Seo'
+
 import { clsxm } from '@/lib'
 
 import Footer from './Footer'
 import Header from './Header'
 import Subscribe from './Subscribe'
 
-type LayoutProps = {
+export interface LayoutProps extends CustomSeoProps {
   customHeader?: JSX.Element
   children: React.ReactNode
 }
 
-const Layout: React.FunctionComponent<LayoutProps> = ({ children, customHeader }) => {
+const Layout: React.FunctionComponent<LayoutProps> = ({ children, customHeader, ...props }) => {
   return (
     <>
+      <Seo {...props} />
       <Header customHeader={customHeader} />
       <main className={clsxm('layout', 'mt-16')}>
         {children}
